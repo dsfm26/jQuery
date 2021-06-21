936da16773bae2035154b1cc49ccf90fd16e8833
David Shim <dsfm26@hotmail.com>
2021-06-21 23:47:30 +0900

    set event listeners using jQuery

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+// set event listener for clicking on h1
 $("h1").click(function(){
     $("h1").css("color", "purple");
 })
 
+// set event listener for clicking on button
 $("button").click(function(){ // no need for a for-loop using jQuery
     $("h1").css("color", "purple");
+})
+
+// set event listener for keystroke on page
+$(document).keypress(function(event){
+    $("h1").text(event.key);
+})
+
+// set event listener for anything (Most generic, mouse over in this case)
+$("h1").on("mouseover", function(){
+    $("h1").css("color", "green");
 })
